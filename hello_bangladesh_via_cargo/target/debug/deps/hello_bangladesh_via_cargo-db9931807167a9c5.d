9f9c2bb1ebc3fe786ad12a666585a4a8ee804525
pdfarhad <pdcsedu@gmail.com>
2023-05-24 10:27:34 +0600

    Hello Bangladesh

--- hello_bangladesh_via_cargo/target/debug/deps/hello_bangladesh_via_cargo-db9931807167a9c5.d
+++
@@ -1,5 +0,0 @@
-/Users/farhadnaeem/Works/Rust/hands-on-rust/hello_bangladesh_via_cargo/target/debug/deps/hello_bangladesh_via_cargo-db9931807167a9c5: src/main.rs
-
-/Users/farhadnaeem/Works/Rust/hands-on-rust/hello_bangladesh_via_cargo/target/debug/deps/hello_bangladesh_via_cargo-db9931807167a9c5.d: src/main.rs
-
-src/main.rs:
--- hello_bangladesh_via_cargo/target/debug/hello_bangladesh_via_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/farhadnaeem/Works/Rust/hands-on-rust/hello_bangladesh_via_cargo/target/debug/hello_bangladesh_via_cargo: /Users/farhadnaeem/Works/Rust/hands-on-rust/hello_bangladesh_via_cargo/src/main.rs
